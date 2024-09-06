3f922f37f0d79466ab5a5bfaf5b1d6586d3df74d
Tibbs <terryjw.97@gmail.com>
2024-09-06 16:11:29 +0100

    Finished sprint

---
+++ index.js
@@ -0,0 +1,5 @@
+const favArtist = document.getElementById("em-caption");
+const favDrink = document.getElementById("tea-caption");
+
+favArtist.innerHTML += "Eminem";
+favDrink.innerHTML += "Yorkshire Tea";
